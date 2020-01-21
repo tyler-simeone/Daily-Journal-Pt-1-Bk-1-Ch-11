2c5da3543a47caef6b48e3793e048710a336b5e2
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-21 12:55:40 -0600

    daily journal 3 completed

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,27 +1,43 @@
-const journalEntry = {
-    date: "01-19-2020",
-    concepts: "Data collection with objects",
-    entry: "In this exercise we covered how to record data input using objects and their properties.",
-    mood: "happy"
+const journalEntries = [
+    {
+        date: "01-19-2020",
+        concepts: "Data collection with objects",
+        entry: "In this exercise we covered how to record data input using objects and their properties.",
+        mood: "happy"
+    },
+    {
+        date: "01-19-2020",
+        concepts: "Accessing nested data in objects",
+        entry: "In this exercise we learned how to access deeply-nested data by chaining properties and array-index selectors to obtain the correct location in the data structure.",
+        mood: "happy"
+    },
+    {
+        date: "01-19-2020",
+        concepts: "Git workflow",
+        entry: "In this exercise we covered how to use git and github to save versions of our codebase to the github cloud, so that we won't lose our hard work and so that we have a reference to previous code versions if we need them.",
+        mood: "happy"
+    }
+];
+
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+        <h2>${journalEntry.concepts}</h2>
+        <p>${journalEntry.entry}</p>
+        <p>${journalEntry.date}</p>
+    `
 };
 
-const journalEntry_2 = {
-    date: "01-19-2020",
-    concepts: "Accessing nested data in objects",
-    entry: "In this exercise we learned how to access deeply-nested data by chaining properties and array-index selectors to obtain the correct location in the data structure.",
-    mood: "happy"
-};
+// for (let i = 0; i < journalEntries.length; i++) {
+//     const entries = makeJournalEntryComponent(journalEntries[i]);
+//     console.log(entries);
+// }
 
-const journalEntry_3 = {
-    date: "01-19-2020",
-    concepts: "Git workflow",
-    entry: "In this exercise we covered how to use git and github to save versions of our codebase to the github cloud, so that we won't lose our hard work and so that we have a reference to previous code versions if we need them.",
-    mood: "happy"
-};
-let journalEntries = [];
+const containerEl = document.querySelector(".entryLog");
 
-journalEntries.push(journalEntry);
-journalEntries.push(journalEntry_2);
-journalEntries.push(journalEntry_3);
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < journalEntries.length; i++) {
+        containerEl.innerHTML += makeJournalEntryComponent(entries[i]);
+    }
+}
 
-// console.log(journalEntries);
+renderJournalEntries(journalEntries);
