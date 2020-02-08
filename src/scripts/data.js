4c36fb50393ff07074db1126f5384974ed29c7a1
Tyler Simeone <56459351+tyler-simeone@users.noreply.github.com>
2020-02-08 05:54:46 -0600

    Ts add delete functionality (#10)
    * working on delete request
    
    * trying to update DOM display
    
    * fixed filtered data display

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,24 +1,40 @@
+import {FACTORY, HTML} from "./entryComponent.js"
+import {DOM, containerEl} from "./entriesDOM.js"
+
 /* 
     This is strictly 'Helper' code. It is not this file's 
-    responsibility to execute the code. 
-    
-    Holds the piece that gets the data from the JSON server.
+    responsibility to execute the code.
 */
 
-    const API = {
-        getJournalEntries () {
-            return fetch("http://localhost:8088/entries")
-                .then(response => response.json())
-        },
-        saveJournalEntry (obj) {
-            return fetch("http://localhost:8088/entries", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(obj)
-            }).then(r=>r.json())
-        }
+// Controls DB connections to my web page
+const API = {
+    getJournalEntries () {
+        return fetch("http://localhost:8088/entries")
+            .then(response => response.json())
+    },
+    getSomeJournalEntries(id) {
+        return fetch(`http://localhost:8088/entries/${id}`)
+            .then(resp => resp.json())
+            .then(entry => {
+                const entryAsHtml = HTML.makeJournalEntryComponent(entry)
+                DOM.renderJournalEntries(entryAsHtml)
+            })
+            
+    },
+    saveJournalEntry (obj) {
+        return fetch("http://localhost:8088/entries", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        }).then(r=>r.json())
+    },
+    rmJournalEntry(recipeId) {
+        return fetch(`http://localhost:8088/entries/${recipeId}`, {
+            method: "DELETE"  
+        });
     }
+}
 
-    export default API  
+export default API  
--- src/scripts/entriesDOM.js
+++ src/scripts/entriesDOM.js
@@ -1,17 +1,10 @@
-/* 
-    This is strictly 'Helper' code. It is not this file's 
-    responsibility to execute the code. 
-    
-    Holds the piece that adds the DOM component to the page to
-    make the JSON data visible.
-*/
-
 const containerEl = document.querySelector(".entryLog");
 
 const DOM = {
     renderJournalEntries(jsonData) {
         containerEl.innerHTML += jsonData;
     }
-}
+} 
 
-export default DOM
+// export default DOM
+export {DOM, containerEl}
